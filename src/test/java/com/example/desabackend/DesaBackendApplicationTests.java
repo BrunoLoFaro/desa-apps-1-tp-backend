58518609369a3e60ca4dbba1f8753636a7e5e63a
Gabriel Aran <gabitoaran@gmail.com>
2026-03-18 19:36:02 -0300

    New SpringBoot project

---
+++ src/test/java/com/example/desabackend/DesaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
